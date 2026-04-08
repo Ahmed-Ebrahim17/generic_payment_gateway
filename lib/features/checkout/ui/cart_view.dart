81550f23f9fe662b6cb0606adb4f08974454ac2a
Ahmed-Ebrahim17 <136754431+Ahmed-Ebrahim17@users.noreply.github.com>
2026-04-08 19:30:45 +0200

    setup project

---
+++ lib/features/checkout/ui/cart_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Cart'),
+      ),
+      body: const Center(
+        child: Text('This is the cart view'),
+      ),
+    );
+  }
+}
